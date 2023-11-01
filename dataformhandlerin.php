f08bcdcd4a9aea20c9eb53368b8ef42ce5f1b24b
thesh0ver <100710819+thesh0ver@users.noreply.github.com>
2023-11-01 15:31:15 -0400

    this php stuff is odd wuman

--- dataformhandlerin.php
+++ dataformhandlerin.php
@@ -1,4 +1,17 @@
 <?php
+$servername = "localhost";
+$username = "admin";
+$password = "vaginavaginav@gin@vag9avaginaV@G1N@";
+$dbname = "pfdb";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    $username = "root";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+}
+// SQL statement to insert data in database
+$sql ="";
 
 ?>
