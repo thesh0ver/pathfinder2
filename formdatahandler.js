d0021d505cd2ba3c68207d639ff1cf7d07f3ff53
himemushi <142841031+himemushi@users.noreply.github.com>
2023-11-05 14:47:51 -0500

    updated js file
    Added process_forms functions for activity and overdest in formdatahandler.js

--- formdatahandler.js
+++ formdatahandler.js
@@ -1,11 +1,11 @@
 function data_thrower(){
-
+   
 }
 function process_dest_form(){
     var nameofdest = document.getElementById("nameofdest").value;
     var typeofdest = document.getElementById("typeofdest").value;
     var releweblink = document.getElementById("releweblink").value;
-    var userlocation = document.getElementById("userlocation").value;
+    var location = document.getElementById("location").value;
     //var x = document.getElementById("x").value;
     // https://stackoverflow.com/questions/45305680/covert-form-data-to-json-string
     let form = document.forms["dest_form"];
@@ -34,6 +34,42 @@ function process_trav_form(){
     //var x = document.getElementById("x").value;
     // https://stackoverflow.com/questions/45305680/covert-form-data-to-json-string
     let form = document.forms["trav_form"];
+    let fd = new FormData(form);
+    let data = {};
+    for (let [key, prop] of fd) {
+        data[key] = prop;
+    }
+    data = JSON.stringify(data, null, 2);
+    console.log(data);
+}
+
+
+function process_acti_form(){
+    var nameofacti = document.getElementById("nameofacti").value;
+    var dateofacti = document.getElementById("dateofacti").value;
+    var descofacti = document.getElementById("descofacti").value;
+    //var x = document.getElementById("x").value;
+    // https://stackoverflow.com/questions/45305680/covert-form-data-to-json-string
+    let form = document.forms["activity_form"];
+    let fd = new FormData(form);
+    let data = {};
+    for (let [key, prop] of fd) {
+        data[key] = prop;
+    }
+    data = JSON.stringify(data, null, 2);
+    console.log(data);
+}
+
+function process_overdest_form(){
+    var nameofdest = document.getElementById("nameofdest").value;
+    var typeofdest = document.getElementById("typeofdest").value;
+    var releweblink = document.getElementById("releweblink").value;
+    var dateofarri = document.getElementById("dateofarri").value;
+    var dateofdept = document.getElementById("dateofdept").value;
+    var userlocation = document.getElementById("location").value;
+    //var x = document.getElementById("x").value;
+    // https://stackoverflow.com/questions/45305680/covert-form-data-to-json-string
+    let form = document.forms["dest_form"];
     let fd = new FormData(form);
     let data = {};
     for (let [key, prop] of fd) {
